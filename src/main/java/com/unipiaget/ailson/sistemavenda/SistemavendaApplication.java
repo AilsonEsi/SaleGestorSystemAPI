1c72d932d6126492dba48e08bf329f3af46bf200
Ailson Moreira <ailsonesi96@gmail.com>
2019-12-31 17:54:17 -0100

    Inital commit

---
+++ src/main/java/com/unipiaget/ailson/sistemavenda/SistemavendaApplication.java
@@ -0,0 +1,13 @@
+package com.unipiaget.ailson.sistemavenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemavendaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemavendaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unipiaget/ailson/sistemavenda/SistemavendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unipiaget.ailson.sistemavenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemavendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
